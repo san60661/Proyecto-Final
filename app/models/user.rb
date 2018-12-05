7b6de3015ecdec9b50eec082c392543d9a7a1264
Francisco <franciscopavon68@gmail.com>
2018-12-05 18:46:15 -0300

    Se agrego restricciones a cargar tarjeta y cvv

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,7 @@ class User < ActiveRecord::Base
 	has_many :inscriptions
     has_many :auctions, :through => :inscriptions
     has_many :reserves, class_name: "Reserve"
+
+    validates :cvv, length:  {minimum: 3, maximum: 3, too_long: "Debe ser de 3 digitos", too_short: "Debe ser de 3 digitos"}
+    validates :card, length:  {minimum: 3, maximum: 3, too_long: "Debe ser de 16 digitos", too_short: "Debe ser de 16 digitos"}
 end
