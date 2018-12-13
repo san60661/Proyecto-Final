719a204124fbadd7c7ab26082ff0142f7b094f43
Juan Manuel <rodrjm94@gmail.com>
2018-12-13 17:33:45 -0300

    Cancelar reserva agregado

--- app/controllers/reserves_controller.rb
+++ app/controllers/reserves_controller.rb
@@ -1,4 +1,8 @@
 class ReservesController < ApplicationController
+	def index
+		@reserves = current_user.reserves
+	end
+	
 	def new
 		@residence = Residence.find(params[:id])
 		@reserve = Reserve.new 
@@ -13,6 +17,16 @@ class ReservesController < ApplicationController
 		else
 			redirect_to new_reserf_path(id: Residence.find(@reserve.residence_id).id), alert: "ERROR: La residencia ya está reservada en esa fecha"
 		end
+	end
+
+	def destroy
+		reserve = Reserve.find(params[:id])
+
+		if reserve.destroy
+			redirect_to reserves_path, notice: "Se canceló la reserva correctamente"
+		else
+			redirect_to reserves_path, notice: "ERROR al cancelar la reserva"	
+		end	
 	end
 
 	private
--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,7 @@ Rails.application.routes.draw do
 
 	resources :users
 
-	resources :reserves, only: [:new, :create]
+	resources :reserves
 
 	resources :inscriptions, only: [:new, :create]
 
