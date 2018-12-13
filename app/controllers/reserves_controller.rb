6119494ed6db3c228c70cad9773d5d57fb03bdf4
san60661 <san60661@gmail.com>
2018-12-13 16:03:14 -0300

    mirar creditos en adjudicar

--- app/controllers/reserves_controller.rb
+++ app/controllers/reserves_controller.rb
@@ -1,13 +1,14 @@
 class ReservesController < ApplicationController
 	def new
 		@residence = Residence.find(params[:id])
-		@reserve = Reserve.new
+		@reserve = Reserve.new 
 	end
 
 	def create
 		@reserve = Reserve.new(params.require(:reserve).permit(:date, :user_id, :residence_id))
 
 		if @reserve.save
+			User.find(current_user.id).update(credits: current_user.credits-1)
 			redirect_to usersReserves_path, notice: "Se confirmo la reserva correctamente"
 		else
 			redirect_to new_reserf_path(id: Residence.find(@reserve.residence_id).id), alert: "ERROR: La residencia ya está reservada en esa fecha"
